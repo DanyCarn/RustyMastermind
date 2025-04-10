2a43b9e8d806323f2e7d3938a62f14320b2866ee
DanyCarn <pc23wsc@eduvaud.ch>
2025-04-10 09:05:44 +0200

    function generating goal added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io::{self, Write};
+use rand::random_range;
+use colored::*;
+
+const MAX_GUESSES: usize = 10;
+const GOAL_LENGTH: usize = 4;
+const NBR_COLORS: usize = 5;
+const POSSIBILITIES: [char; NBR_COLORS] = ['R', 'G', 'B', 'W', 'Y'];
+
+//Generates the goal code
+fn generate_code(possibilities: [char; NBR_COLORS]) -> [char; GOAL_LENGTH]{
+
+    let mut goal: [char; GOAL_LENGTH] = ['a'; GOAL_LENGTH];
+
+    for x in 0..GOAL_LENGTH{
+
+        goal[x] = possibilities[random_range(1..possibilities.len())];
+    }
+
+    goal
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let goal = generate_code(POSSIBILITIES);
+
+    for x in goal {
+        println!("{x}");
+    }
 }
